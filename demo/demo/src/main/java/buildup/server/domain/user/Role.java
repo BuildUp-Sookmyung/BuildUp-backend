958b0783b69676fb7c2371da8c5ced7479fb657c
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-04 14:12:15 +0900

    feat:>add role setter/#1

--- demo/demo/src/main/java/buildup/server/domain/user/Role.java
+++ demo/demo/src/main/java/buildup/server/domain/user/Role.java
@@ -2,8 +2,10 @@ package buildup.server.domain.user;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 public enum Role {
 
