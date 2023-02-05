e62cd89019f39fb0e7434613a3d2525987e59606
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-06 03:59:16 +0900

    fix:> fix LocalLoginReqestDto add nickname field/#1

--- demo/demo/src/main/java/buildup/server/dto/LocalJoinRequest.java
+++ demo/demo/src/main/java/buildup/server/dto/LocalJoinRequest.java
@@ -2,6 +2,7 @@ package buildup.server.dto;
 
 
 import buildup.server.domain.user.Member;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,12 +11,18 @@ import java.time.LocalDate;
 @Data
 public class LocalJoinRequest {
     private final PasswordEncoder passwordEncoder;
+
+    @NotBlank
+    private String nickname;
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
     private String phone;
 
     public Member toEntity() {
         return Member.builder()
+                .nickname(nickname)
                 .username(username)
                 .password(passwordEncoder.encode(password))
                 .build();
--- demo/demo/src/main/java/buildup/server/dto/LocalLoginRequest.java
+++ demo/demo/src/main/java/buildup/server/dto/LocalLoginRequest.java
@@ -2,7 +2,7 @@ package buildup.server.dto;
 import lombok.Data;
 
 @Data
-public class LocalLoginRequestDto {
+public class LocalLoginRequest {
     private final String username;
     private final String password;
 }
