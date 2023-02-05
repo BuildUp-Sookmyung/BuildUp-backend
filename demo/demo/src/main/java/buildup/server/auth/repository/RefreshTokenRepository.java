2e57a1cb273d5e7f305433aad218ab6ef28d2acd
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-06 03:31:33 +0900

    feat:> RefreshTokenRepository/#1

--- demo/demo/src/main/java/buildup/server/auth/repository/RefreshTokenRepository.java
+++ demo/demo/src/main/java/buildup/server/auth/repository/RefreshTokenRepository.java
@@ -4,4 +4,7 @@ import buildup.server.auth.domain.MemberRefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<MemberRefreshToken, Long> {
+
+    MemberRefreshToken findByUserId(String userId);
+    MemberRefreshToken findByUserIdAndRefreshToken(String userId, String refreshToken);
 }
