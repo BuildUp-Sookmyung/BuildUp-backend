f0be0c3f6e902718cf6f03bb9e2ecb3201f0486b
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-07 14:29:12 +0900

    feat:> Category Entity/#10

---
+++ demo/demo/src/main/java/buildup/server/entity/Category.java
@@ -0,0 +1,24 @@
+package buildup.server.entity;
+
+import buildup.server.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
