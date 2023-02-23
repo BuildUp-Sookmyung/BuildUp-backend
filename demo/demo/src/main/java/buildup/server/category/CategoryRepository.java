a880ef4bd3a51a7956500a7545c55d0cfa669cb1
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-23 12:05:59 +0900

    feat:> category repository/#103

---
+++ demo/demo/src/main/java/buildup/server/category/CategoryRepository.java
@@ -0,0 +1,11 @@
+package buildup.server.category;
+
+import buildup.server.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByMember(Member member);
+}
