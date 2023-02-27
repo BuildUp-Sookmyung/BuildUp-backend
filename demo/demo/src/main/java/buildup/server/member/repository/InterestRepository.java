20f9026ed268c6833a1d9e469339e6fa75a0f4bf
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-27 15:27:33 +0900

    feat:> InterestRepository new method for search profiles/#110

--- demo/demo/src/main/java/buildup/server/member/repository/InterestRepository.java
+++ demo/demo/src/main/java/buildup/server/member/repository/InterestRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
-    @Query(value = "select i from Interest I where i.profile.id =: profileId", nativeQuery = true)
-    List<Interest> findAllByProfileId(Long profileId);
+    List<Interest> findAllByFieldContaining(String keyword);
 }
